8f0df9e7ff82319985315b1e51616e8fe826722f
Sarthaksinghal987 <101854170+Sarthaksinghal987@users.noreply.github.com>
2024-01-18 02:24:47 +0530

    Update file.js

--- models/file.js
+++ models/file.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
-mongoose.set('strictQuery', false);
+//This imports the Mongoose library, which is a MongoDB ODM (Object Data Modeling) library for Node.js.
 
 const Schema = mongoose.Schema;
+//This creates a reference to the Schema constructor from the Mongoose library. It will be used to define the structure of the documents in the MongoDB collection.
 
 const fileSchema = new Schema({
     filename: {type: String, required: true },
@@ -13,5 +13,9 @@ const fileSchema = new Schema({
     receiver: { type: String, required: false },
 
 }, { timeStamps: true});
+//This defines the schema for the "File" model.
+//The second argument to new Schema(..., { timestamps: true }) enables Mongoose to automatically manage createdAt and updatedAt fields in each document, providing timestamps for when the document was created and last updated.
 
 module.exports = mongoose.model('File', fileSchema);
+//This exports the Mongoose model for the "File" schema. 
+//The model is named "File" and will be used to interact with the "files" collection in the connected MongoDB database.
